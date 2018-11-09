bf24378a26bdc34dc5d5ebeec30ca7fb51b3459a
Omodot <omodote@hotmail.com>
2018-11-09 13:39:11 +0100

    first commit

---
+++ src/eniola/Eniola.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eniola;
+
+/**
+ *
+ * @author omodo_000
+ */
+public class Eniola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login lg=new login();
+        lg.setVisible(true);
+        lg.setSize(536, 567);
+    }
+    
+}
